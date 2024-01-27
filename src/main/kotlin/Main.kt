19da920be113d37adbbeffd89fb9a9da54c45df2
Maher Hanna <maher.hn802@gmail.com>
2024-01-27 16:37:12 +0200

    Get random positions from file when comparing tow engines

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -28,7 +28,7 @@ fun main(args: Array<String>) {
         }
 
         2 -> {
-            compareEngines(args[0],args[1])
+            compareEngines(args[0], args[1])
         }
 
         else -> {
@@ -41,12 +41,33 @@ fun main(args: Array<String>) {
 
 }
 
-fun compareEngines(firstEnginePath: String, secondEnginePath: String){
+fun compareEngines(firstEnginePath: String, secondEnginePath: String) {
+    var carlsenGamesBufferReader =
+        object {}.javaClass.getResourceAsStream("/games_database/carlsen.txt")?.bufferedReader()
+    var positions = mutableListOf<String>()
+    try {
+
+        var line: String?
+        while (carlsenGamesBufferReader?.readLine().also { line = it } != null) {
+            // Process each line
+            if (line != "\u0000") {
+                line?.let { positions.add(it.trim().replace("\n".toRegex(), replacement = "")) }
+            }
+        }
+    } catch (e: Exception) {
+        println("An error occurred: ${e.message}")
+    } finally {
+        try {
+            carlsenGamesBufferReader?.close()
+        } catch (e: Exception) {
+            println("An error occurred while closing the file: ${e.message}")
+        }
+    }
     val firstJarLoader = JarLoader()
     firstJarLoader.loadEngineJar(firstEnginePath)
     val secondJarLoader = JarLoader()
     secondJarLoader.loadEngineJar(secondEnginePath)
-    val numberOfGames = 20
+    val numberOfGames = 30
     var currentGameNumber = 1
     var isCurrentGameFinished: Boolean
     var movesList: String
@@ -60,37 +81,51 @@ fun compareEngines(firstEnginePath: String, secondEnginePath: String){
             val random = Random(System.currentTimeMillis())
             val randomPlayerIndex = random.nextInt(2)
             var currentPlayerIndex = randomPlayerIndex
-            if(currentPlayerIndex == 0){
-                firstJarLoader.commandEngine("position startpos")
+            val randomPosition =
+                positions[random.nextInt(positions.size)] ?: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 "
+            if (currentPlayerIndex == 0) {
+                firstJarLoader.commandEngine("position fen $randomPosition")
+                val gameStatusResponse = firstJarLoader.commandEngine("check_status")
+                // game status : 0 not finished, 1 draw, 2 white winds, 3 black wins
+                val statusCode = gameStatusResponse.toInt()
+                if (statusCode != 0) {
+                    continue
+                }
                 val move = firstJarLoader.commandEngine("go infinite")
                 val splits = move.trim().split("\\s+".toRegex())
-                if(splits.size > 1){
+                if (splits.size > 1) {
                     movesList += splits[1] + " "
                 }
             } else {
-                secondJarLoader.commandEngine("position startpos")
+                secondJarLoader.commandEngine("position fen $randomPosition")
+                val gameStatusResponse = firstJarLoader.commandEngine("check_status")
+                // game status : 0 not finished, 1 draw, 2 white winds, 3 black wins
+                val statusCode = gameStatusResponse.toInt()
+                if (statusCode != 0) {
+                    continue
+                }
                 val move = secondJarLoader.commandEngine("go infinite")
                 val splits = move.trim().split("\\s+".toRegex())
-                if(splits.size > 1){
+                if (splits.size > 1) {
                     movesList += splits[1] + " "
                 }
             }
-            while (!isCurrentGameFinished){
+            while (!isCurrentGameFinished) {
                 // flip current player index from 0 to 1 and vice versa
                 currentPlayerIndex = currentPlayerIndex xor 1
 
-                if(currentPlayerIndex == 0){
-                    firstJarLoader.commandEngine("position startpos moves $movesList")
+                if (currentPlayerIndex == 0) {
+                    firstJarLoader.commandEngine("position fen $randomPosition moves $movesList")
                     val move = firstJarLoader.commandEngine("go infinite")
                     val splits = move.trim().split("\\s+".toRegex())
-                    if(splits.size > 1){
+                    if (splits.size > 1) {
                         movesList += splits[1] + " "
                     }
-                } else{
-                    secondJarLoader.commandEngine("position startpos moves $movesList")
+                } else {
+                    secondJarLoader.commandEngine("position fen $randomPosition moves $movesList")
                     val move = secondJarLoader.commandEngine("go infinite")
                     val splits = move.trim().split("\\s+".toRegex())
-                    if(splits.size > 1){
+                    if (splits.size > 1) {
                         movesList += splits[1] + " "
                     }
                 }
@@ -99,20 +134,21 @@ fun compareEngines(firstEnginePath: String, secondEnginePath: String){
                 // game status : 0 not finished, 1 draw, 2 white winds, 3 black wins
                 val statusCode = gameStatusResponse.toInt()
                 isCurrentGameFinished = statusCode != 0
-                if(isCurrentGameFinished){
-                    when(statusCode){
+                if (isCurrentGameFinished) {
+                    when (statusCode) {
                         1 -> firstEngineDraws += 1
                         2 -> {
-                            if(randomPlayerIndex == 0){
+                            if (randomPlayerIndex == 0) {
                                 firstEngineWins += 1
-                            }else {
+                            } else {
                                 firstEngineLoses += 1
                             }
                         }
+
                         3 -> {
-                            if(randomPlayerIndex == 0){
+                            if (randomPlayerIndex == 0) {
                                 firstEngineLoses += 1
-                            }else {
+                            } else {
                                 firstEngineWins += 1
                             }
                         }
@@ -127,7 +163,10 @@ fun compareEngines(firstEnginePath: String, secondEnginePath: String){
         println("System.in was closed; exiting")
     } catch (e: NoSuchElementException) {
         println("System.in was closed; exiting")
-
+    }catch (e: Exception){
+        println("Error occurred:")
+        println("First engine results compared to second engine are:")
+        println("wins: $firstEngineWins draws: $firstEngineDraws loses: $firstEngineLoses")
     }
 }
 
